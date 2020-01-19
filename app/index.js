21773ac40bb723024287344be33726f70a8692d0
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2020-01-19 17:36:17 -0500

    🐛 add text-length to Ionix css file
    Might be due to the new SDK; now the absent text-length field in the
    Ionic css causes bad truncation of date and time fields 😞.
    
    Add the text-length specifier. Verified working again.
    
    Fixes #10.

--- app/index.js
+++ app/index.js
@@ -47,7 +47,7 @@ clock.ontick = (evt) => {
   ];
   const shortMonthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
   "July", "Aug", "Sept", "Oct", "Nov", "Dec"
-];
+  ];
   const dayOfWeekNames = ["Sunday", "Monday", "Tuesday", "Wednesday",
     "Thursday", "Friday", "Saturday"
   ];
